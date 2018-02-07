01bf01a67de0fb64055b7204c02bb4e5059c677e
ore21 <35785030+ore21@users.noreply.github.com>
2018-02-07 17:13:18 -0600

    Add files via upload

--- age classifier.py
+++ age classifier.py
@@ -1,15 +1,12 @@
-print("Age Classifier")
+"""Age Classifier"""
 
-person_age = (float(input("age")))
+person_age = (int(input("age? ")))
 
-if person_age <= 1 or person_age > 0:
+if 0 < person_age <= 1:
     print("person is an infant")
-
-elif person_age > 1 or person_age < 13:
+elif 13 > person_age > 1:
     print("person is a child")
-
-elif person_age >= 13 or person_age < 20:
+elif 20 > person_age >= 13:
     print("person is a teenager")
-
 elif person_age >= 20:
     print("person is an adult")
