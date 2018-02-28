4974202ff7a327fe8371af6497124e2712641a10
ore21 <35785030+ore21@users.noreply.github.com>
2018-02-28 17:29:00 -0600

    Add files via upload

--- average of numbers.py
+++ average of numbers.py
@@ -12,16 +12,16 @@ def main():
         num_line = object_file.readline()
 
     count = 0
-
     total = 0
 
     for num_line in object_file:
-
         total += int(num_line)
-
         count += 1
+        print("the total of numbers is :", total, " .2f ")
+        avg = (total / count)
+        print("total of numbers is :", avg, " .2f ")
 
-    print(total / count)
+        total = object_file.readline(total + count)
 
     object_file.close()
 
