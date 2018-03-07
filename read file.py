36709e51bafe8b1b1b5d2b6181a0cf1bcc48c883
ore21 <35785030+ore21@users.noreply.github.com>
2018-03-07 16:59:43 -0600

    Add files via upload

--- read file.py
+++ read file.py
@@ -3,14 +3,18 @@
 
 def names():
     input_file = open("names.txt", "r")
-
     num = input_file.readline()
+    total = 0
 
     while num != "":
         print(num)
         num = input_file.readline()
+        total += 1
 
     input_file.close()
+    print("you had total of :" + num)
+    print(total)
+    print("names ")
 
 
 names()
