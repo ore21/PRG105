8e449484af6701d9ffec5c002bf69c14490a03aa
ore21 <35785030+ore21@users.noreply.github.com>
2018-03-10 08:23:16 -0600

    Add files via upload

---
+++ popular names.py
@@ -0,0 +1,40 @@
+"""popular names"""
+
+
+def main():
+    input_file = open('BoyNames.txt', 'r')
+    boy_names = input_file.readlines()
+
+    input_file.close()
+
+    index = 0
+    while index < len(boy_names):
+        boy_names[index] = boy_names[index].rstrip('\n')
+        index += 1
+
+    print(boy_names)
+
+    if 'Ryan' in boy_names:
+        print('yes, is on the popular list. ')
+    else:
+        print('no, is not on the popular list. ')
+
+    input_file = open('GirlNames.txt', 'r')
+    girl_names = input_file.readlines()
+
+    input_file.close()
+
+    index = 0
+    while index < len(girl_names):
+        girl_names[index] = girl_names[index].rstrip('\n')
+        index += 1
+
+    print(girl_names)
+
+    if 'Camila' in girl_names:
+        print('yes, is on the popular list. ')
+    else:
+        print('no, is not on the popular list. ')
+
+
+main()
