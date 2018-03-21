20486630928881cc65797399b294a969047d9f68
ore21 <35785030+ore21@users.noreply.github.com>
2018-03-21 18:57:18 -0500

    Add files via upload

--- list processing.py
+++ list processing.py
@@ -1,24 +1,21 @@
 """list processing"""
-
-my_randoms = 0
-number = 0
+import random
 
 
 def main():
+    my_random = []
     for i in range(20):
-        my_randoms = random.ranrange(range(1, 101, 1))
-        return my_randoms
-        print(my_randoms)
+        my_random.append(random.randrange(1, 101, 1))
+
+    random_number = numbers()
+    my_random.append(random_number)
+    print(my_random)
+
 
 def numbers():
-    print("Enter a number between 1 - 100 :")
-    print(my_randoms)
-
-    if number == (1, 101, 1):
-        print("number is greater then user's number :")
-
-    elif number != (1, 101, 1):
-        print(" there are no numbers in the list greater than the entered number :")
+    user_num = input("Enter a number between 1 - 100 :")
+    print(user_num)
+    return user_num
 
 
 main()
