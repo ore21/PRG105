7674b6674a26751be41a8f8e57631358e8cd4955
ore21 <35785030+ore21@users.noreply.github.com>
2018-04-16 16:28:54 -0500

    Add files via upload

--- Recursive list sum.py
+++ Recursive list sum.py
@@ -2,12 +2,13 @@
 
 
 def main():
-    num_list1 = [3, 4, 5, 7, 9, 12]
-    num(num_list1)
+    num_list = [3, 4, 5, 7, 9, 12]
+    num(num_list)
 
 
 def num(num_list):
     if len(num_list) > 1:
+        print(num_list[0])
         num_list[0] += num_list[1]
         num_list.pop(1)
         num(num_list)
