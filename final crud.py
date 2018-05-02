478927314bd67536c92f0ce279aef5f38a808b31
ore21 <35785030+ore21@users.noreply.github.com>
2018-05-01 19:15:52 -0500

    Add files via upload

--- final crud.py
+++ final crud.py
@@ -15,17 +15,19 @@ class CrudGUI:
 
         self.radio_var = tkinter.IntVar()
         self.radio_var.set(1)
-
         # create the radio buttons
         self.look = tkinter.Radiobutton(self.top_frame, text='Look up customer',
+
                                         variable=self.radio_var, value=1)
         self.add = tkinter.Radiobutton(self.top_frame, text='Add Customer',
+
                                        variable=self.radio_var, value=2)
         self.change = tkinter.Radiobutton(self.top_frame, text='Change customer email',
+
                                           variable=self.radio_var, value=3)
         self.delete = tkinter.Radiobutton(self.top_frame, text='Delete customer',
-                                          variable=self.radio_var, value=4)
 
+                                          variable=self.radio_var, value=4)
         # pack the radio buttons
         self.look.pack(anchor='w', padx=20)
         self.add.pack(anchor='w', padx=20)
@@ -33,8 +35,9 @@ class CrudGUI:
         self.delete.pack(anchor='w', padx=20)
 
         # create ok and quit buttons
+
         self.ok_button = tkinter.Button(self.bottom_frame, text='OK', command=self.open_menu)
-        self.quit_button = tkinter.Button(self.bottom_frame, text='Save & QUIT', command=self.save)
+        self.quit_button = tkinter.Button(self.bottom_frame, text='Save & QUIT', command=self.master.save)
 
         # pack the buttons
         self.ok_button.pack(side='left')
@@ -47,19 +50,25 @@ class CrudGUI:
     def open_menu(self):
         if self.radio_var.get() == 1:
             search = LookGUI(self.master, self.customer_list)
+
         elif self.radio_var.get() == 2:
             add = AddGUI(self.master, self.customer_list)
+
         elif self.radio_var.get() == 3:
             change = ChangeGUI(self.master, self.customer_list)
+
         elif self.radio_var.get() == 4:
             delete = DeleteGUI(self.master, self.customer_list)
+
         else:
             tkinter.messagebox.showinfo('Function', 'still under construction')
 
     def save(self):
-        input_file = open('customer_file.dat', 'rb')
-        customers = pickle.load(input_file)
-
+        print('The info has been updated.')
+        save_file = open('customer_file.dat', 'wb')
+        pickle.dump(self.customer_list, save_file)
+        save_file.close()
+        self.master.destroy()
 
 
 class LookGUI:
@@ -68,6 +77,7 @@ class LookGUI:
         self.look = tkinter.Toplevel(master)
         self.look.title('Search for customer')
         self.customer_list = look_cust
+
         # self.look_window = tkinter.Tk()
         self.top_frame = tkinter.Frame(self.look)
         self.middle_frame = tkinter.Frame(self.look)
@@ -103,10 +113,10 @@ class LookGUI:
         self.middle_frame.pack()
         self.bottom_frame.pack()
 
-        tkinter.mainloop()
-
     def search(self):
-        tkinter.messagebox.showinfo('Search', 'Name has been found')
+        name = self.search_entry.get()
+        result = self.customer_list.get(name, 'Not found')
+        self.value.set(result)
 
     def back(self):
         self.look.destroy()
@@ -121,7 +131,7 @@ class AddGUI:
 
         # self.look_window = tkinter.Tk()
         self.top_frame = tkinter.Frame(self.look)
-        self.middle_frame = tkinter.Frame(self.look)
+        self.info_frame = tkinter.Frame(self.look)
         self.bottom_frame = tkinter.Frame(self.look)
 
         # widgets for top frame
@@ -132,11 +142,11 @@ class AddGUI:
         self.add_label.pack(side='left')
         self.add_entry.pack(side='left')
 
-        #info frame for results
-        self.info_label = tkinter.Label(self.middle_frame, text='Add Name')
-        self.info_entry = tkinter.Entry(self.middle_frame, widht=15)
+        # info frame for results
+        self.info_label = tkinter.Label(self.info_frame, text='Add info')
+        self.info_entry = tkinter.Entry(self.info_frame, widht=15)
 
-        #pack info/middle frame
+        # pack info/middle frame
         self.info_label.pack(side='left')
         self.info_entry.pack(side='left')
 
@@ -150,7 +160,7 @@ class AddGUI:
 
         # pack frames
         self.top_frame.pack()
-        self.middle_frame.pack()
+        self.info_frame.pack()
         self.bottom_frame.pack()
 
     def add(self):
@@ -159,9 +169,9 @@ class AddGUI:
         if name not in self.customer_list:
             self.customer_list[name] = info
             self.look.destroy()
-            tkinter.messagebox.showinfo('Add', 'Name has been added')
+            tkinter.messagebox.showinfo('List', 'Name has been added')
         else:
-            tkinter.messagebox.showinfo('Name is already in the list')
+            tkinter.messagebox.showinfo('List', 'Name is already in the list')
 
     def back(self):
         self.look.destroy()
@@ -170,13 +180,14 @@ class AddGUI:
 class ChangeGUI:
     def __init__(self, master, change_cust):
         self.master = master
-        self.change = tkinter.Toplevel(master)
-        self.change.title('Change a customer')
+        self.look = tkinter.Toplevel(master)
+        self.look.title('Change a customer')
         self.customer_list = change_cust
+
         # self.look_window = tkinter.Tk()
-        self.top_frame = tkinter.Frame(self.change)
-        self.middle_frame = tkinter.Frame(self.change)
-        self.bottom_frame = tkinter.Frame(self.change)
+        self.top_frame = tkinter.Frame(self.look)
+        self.middle_frame = tkinter.Frame(self.look)
+        self.bottom_frame = tkinter.Frame(self.look)
 
         # widgets for top frame
         self.change_label = tkinter.Label(self.top_frame, text='Change a customer name: ')
@@ -187,16 +198,15 @@ class ChangeGUI:
         self.change_entry.pack(side='left')
 
         # middle frame
-        self.value = tkinter.StringVar()
-        self.info = tkinter.Label(self.middle_frame, text='Results: ')
-        self.result_label = tkinter.Label(self.middle_frame, textvariable=self.value)
+        self.info_label = tkinter.Label(self.middle_frame, text='Enter a name to change: ')
+        self.info_entry = tkinter.Label(self.middle_frame, width=15)
 
         # pack Middle frame
-        self.info.pack(side='left')
-        self.result_label.pack(side='left')
+        self.info_label.pack(side='left')
+        self.info_entry.pack(side='left')
 
         # buttons for bottom frame
-        self.change_button = tkinter.Button(self.bottom_frame, text='Add', command=self.change)
+        self.change_button = tkinter.Button(self.bottom_frame, text='Change', command=self.change)
         self.back_button = tkinter.Button(self.bottom_frame, text='Main Menu', command=self.back)
 
         # pack bottom frame
@@ -210,13 +220,13 @@ class ChangeGUI:
 
     def change(self):
         name = self.change_entry.get()
-        info = self.customer_list.get(name, 'Not Found')
+        info = self.info_entry.get()
         if name not in self.customer_list:
             self.customer_list[name] = info
-            self.value.set(info)
-            tkinter.messagebox.showinfo('Change', 'Name has been changed')
+            self.look.destroy()
+            tkinter.messagebox.showinfo('List', 'Name has been changed')
         else:
-            tkinter.messagebox.showinfo('Name has already been changed')
+            tkinter.messagebox.showinfo('List', 'Name cannot be changed changed')
 
     def back(self):
         self.change.destroy()
@@ -225,13 +235,13 @@ class ChangeGUI:
 class DeleteGUI:
     def __init__(self, master, delete_cust):
         self.master = master
-        self.delete = tkinter.Toplevel(master)
-        self.delete.title('Delete a customer')
+        self.look = tkinter.Toplevel(master)
+        self.look.title('Delete a customer')
         self.customer_list = delete_cust
+
         # self.look_window = tkinter.Tk()
-        self.top_frame = tkinter.Frame(self.delete)
-        self.middle_frame = tkinter.Frame(self.delete)
-        self.bottom_frame = tkinter.Frame(self.delete)
+        self.top_frame = tkinter.Frame(self.look)
+        self.bottom_frame = tkinter.Frame(self.look)
 
         # widgets for top frame
         self.delete_label = tkinter.Label(self.top_frame, text='Delete a customer name: ')
@@ -240,15 +250,6 @@ class DeleteGUI:
         # pack top frame
         self.delete_label.pack(side='left')
         self.delete_entry.pack(side='left')
-
-        # middle frame
-        self.value = tkinter.StringVar()
-        self.info = tkinter.Label(self.middle_frame, text='Results: ')
-        self.result_label = tkinter.Label(self.middle_frame, textvariable=self.value)
-
-        # pack Middle frame
-        self.info.pack(side='left')
-        self.result_label.pack(side='left')
 
         # buttons for bottom frame
         self.delete_button = tkinter.Button(self.bottom_frame, text='Delete', command=self.delete)
@@ -260,19 +261,20 @@ class DeleteGUI:
 
         # pack frames
         self.top_frame.pack()
-        self.middle_frame.pack()
         self.bottom_frame.pack()
 
     def delete(self):
         name = self.delete_entry.get()
         if name not in self.customer_list:
             del self.customer_list[name]
-            tkinter.messagebox.showinfo('Deleted', 'Name has been Deleted')
+            self.look.destroy()
+            tkinter.messagebox.showinfo('List', 'Name has been Deleted')
         else:
-            tkinter.messagebox.showinfo('Name cannot be deleted')
+            tkinter.messagebox.showinfo('List', 'Name cannot be deleted')
 
     def back(self):
         self.delete.destroy()
+
 
 def main():
     try:
@@ -283,8 +285,10 @@ def main():
 
     # create a window
     root = tkinter.Tk()
+
     # call the GUI and send it the root menu
     menuGui = CrudGUI(root, customers)
+
     # control the mainloop from main instead of the class
     root.mainloop()
 
